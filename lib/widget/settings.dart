ea2aec84fcf5b2f5d071b0bdb3f9419c14b62f26
Nöll <gl309@uni-siegen.de>
2020-07-09 14:04:33 +0200

    integrates FormWidget into the normal Settings to minmize double-code and async operations

--- lib/widget/settings.dart
+++ lib/widget/settings.dart
@@ -12,32 +12,125 @@ import 'package:qr_translator/storage.dart';
 import 'package:qr_translator/widget/qr-scanner.dart';
 
 
-class FormWidget extends StatefulWidget {
-  @override
-  _FormWidgetState createState() => _FormWidgetState();
+/// ###################### Main Settings Widget ##########################################
+/// 
+/// 
+
+class SettingsWidget extends StatefulWidget {
+
+  
+  _SettingsWidget createState() => _SettingsWidget();
 }
 
-class _FormWidgetState extends State<FormWidget> {
-  final _formKey = GlobalKey<FormState>();
+class _SettingsWidget extends State<SettingsWidget> {
+  bool _littleWidget = false;
+  bool _colorActivated = false;
+  bool _showHelp = true;
+  bool _optOut = false;
+
+  bool _newVersionAvailable = false;
+  bool _adressCorrect = false;
+
   String _adress;
   String _error = "Testing ...";
   String _localVersion;
   String _rawJson;
+
   Map<String, dynamic> _decodedJson;
-  bool _newVersionAvailable = false;
-  bool _adressCorrect = false;
   Color _dataColor = Colors.grey;
   final _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
+  @override
+  void initState() {
+    super.initState();
+    _getPrefs();
+    _fetchScheme(http.Client(), _adress);
+    _controller.addListener(_refetchOnChange);
+    _trackPageview();
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  /// Get SharedPreferences and OptOut Status
+  Future _getPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey("littleWidget")){
+      setState(() {
+        _littleWidget = prefs.getBool("littleWidget");
+      });
+    }
+    if (prefs.containsKey("colorActivated")){
+      setState(() {
+        _colorActivated = prefs.getBool("colorActivated");
+      });
+    }
+    if(prefs.containsKey("showHelp")) {
+      setState(() {
+        _showHelp = prefs.getBool("showHelp");
+      });
+    }
+    if(prefs.containsKey("adress")) {
+      setState(() {
+        _adress = prefs.getString("adress");
+        _controller.text = _adress;
+      });
+    }
+    if(prefs.containsKey("localVersion")) {
+      setState(() {
+        _localVersion = prefs.getString('localVersion');
+      });
+    }
+
+    bool isOptOut = await FlutterMatomo.isOptOut();
+    if (isOptOut != null){
+      setState(() {
+        _optOut = isOptOut;
+      });
+    }
+  }
+  
+  /// toggle a single Value of the SharedPreferences
+  Future _toggleValue(name, value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(name, value);
+  }
+  
+  /// Track the Pageview with Matomo if not OptOut
+  Future<void> _trackPageview() async {
+    await FlutterMatomo.trackScreen(context, "Opened");
+  }
+
+  /// Toggle the Optout of the Matomo Tracking
+  Future _toggleOptOut(bool value) async {
+    await FlutterMatomo.setOptOut(value);
+  }
+
+  /// Launch the Predefined URL in a Browser
+  _launchURL() async {
+    const url = 'https://noell.li';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  /// Refetch on change from the TextController
   void _refetchOnChange() {
     _fetchScheme(http.Client(), _controller.text);
   }
 
-  // Compare the Version Numbers
-  Future _compareVersion(version) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('localVersion') != version;
-  }
+  // // Compare the Version Numbers
+  // Future _compareVersion(version) async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   return prefs.getString('localVersion') != version;
+  // }
 
   // Update the json on filesystem
   Future _updateLocalScheme() async {
@@ -52,7 +145,6 @@ class _FormWidgetState extends State<FormWidget> {
     _getPrefs();
   }
 
-
   // Future to test JSON
   Future _fetchScheme(http.Client client, adress) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -64,10 +156,13 @@ class _FormWidgetState extends State<FormWidget> {
       final _response = await http.get(adress);
       try {
         var _decoded = json.decode(_response.body) as Map<String, dynamic>;
+
+        // if the required Key "Version" is not available throw an Exception
         if (_decoded['Version'] == null) {
           throw FormatException();
         }
-        var _compare =  await _compareVersion(_decoded['Version']);
+
+        var _compare = _localVersion != _decoded['Version']; // maybe switch _localVersion for await prefs.getString('localVersion')
         await prefs.setString('adress', adress);
         setState(() {
           if (_compare) {
@@ -97,32 +192,8 @@ class _FormWidgetState extends State<FormWidget> {
       }); 
     }
   }
-
-  /// get the Preferences
-  Future _getPrefs() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _adress = prefs.getString("adress");
-      _controller.text = _adress;
-      _localVersion = prefs.getString('localVersion');
-    });
-  }
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getPrefs();
-    _fetchScheme(http.Client(), _adress);
-    _controller.addListener(_refetchOnChange);
-  }
-
+  
+  /// QR-Scanner Overlay to Scan a code with a URL to schemes
   void _showOverlay(BuildContext context) async {
     final adress = await Navigator.push(
       context,
@@ -144,7 +215,7 @@ class _FormWidgetState extends State<FormWidget> {
     callback(adress);
   }
 
-
+  /// Callback for the QR-Scanner to pass it's contents to Our TextController
   void callback(String adress) {
     if (adress != null) {
       setState(() {
@@ -154,13 +225,13 @@ class _FormWidgetState extends State<FormWidget> {
       _fetchScheme(http.Client(), this._adress);
     }
   }
-
-
+  
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context) {
     var _width = MediaQuery.of(context).size.width;
     var _button;
     
+    // Check if a new Version is Available and change Button according
     if ( _newVersionAvailable ) {
       _button = RaisedButton(
         color: _dataColor,
@@ -182,132 +253,8 @@ class _FormWidgetState extends State<FormWidget> {
       );
     }
 
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.camera_alt),
-                  onPressed: () {
-                    _showOverlay(context);
-                  },
-                ),
-                border: OutlineInputBorder(),
-                labelText: AppLocalizations.of(context).translate('descriptionToAdress'),
-              ),
-              controller: _controller,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                width: _width / 2,
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _error,
-                      style: TextStyle(color: _dataColor)
-                    ),
-                    Text("${AppLocalizations.of(context).translate('localVersion')}: ${this._localVersion ?? "---"}"),
-                    this._adressCorrect ? Text("${AppLocalizations.of(context).translate('onlineVersion')}: ${this._decodedJson['Version']}") : Text("${AppLocalizations.of(context).translate('onlineVersion')}: ---"),
-                  ],
-                ),
-              ),
-              Container(
-                width: _width /2,
-                padding: EdgeInsets.all(15),
-                child: _button,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
 
-
-
-/// ###################### Main Settings Widget ##########################################
-/// ToDo: integrate FormWidget into the normal Settings to minmize double-code and async operations
-class SettingsWidget extends StatefulWidget {
-
-  
-  _SettingsWidget createState() => _SettingsWidget();
-}
-
-class _SettingsWidget extends State<SettingsWidget> {
-  bool _littleWidget = false;
-  bool _colorActivated = false;
-  bool _showHelp = true;
-  bool _optOut = false;
-
-  Future _toggleValue(name, value) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(name, value);
-  }
-
-  Future _getPrefs() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey("littleWidget")){
-      setState(() {
-        _littleWidget = prefs.getBool("littleWidget");
-      });
-    }
-    if (prefs.containsKey("colorActivated")){
-      setState(() {
-        _colorActivated = prefs.getBool("colorActivated");
-      });
-    }
-    if(prefs.containsKey("showHelp")) {
-      setState(() {
-        _showHelp = prefs.getBool("showHelp");
-      });
-    }
-
-    bool isOptOut = await FlutterMatomo.isOptOut();
-    if (isOptOut != null){
-      setState(() {
-        _optOut = isOptOut;
-      });
-    }
-  }
-
-  Future _toggleOptOut(bool value) async {
-    String version = await FlutterMatomo.setOptOut(value);
-  }
-
-@override
-  void initState() {
-    super.initState();
-    _getPrefs();
-    _initMatomo();
-  }
-
-  Future<void> _initMatomo() async {
-    String version =  await FlutterMatomo.trackScreen(context, "Opened");
-  }
-
-  _launchURL() async {
-    const url = 'https://noell.li';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  @override
-  Widget build (BuildContext context) {
-
-  return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -316,6 +263,7 @@ class _SettingsWidget extends State<SettingsWidget> {
       body: Center(
         child: ListView(
           children: <Widget>[
+            // Header for "Scheme Settings"
             ListTile(
               title: Text(
                 AppLocalizations.of(context).translate('schemeSettings'),
@@ -326,8 +274,57 @@ class _SettingsWidget extends State<SettingsWidget> {
               ),
               subtitle: Text(AppLocalizations.of(context).translate('enterAdress')),
             ),
-            FormWidget(),
+            // Form with TextController to update Schemes
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.camera_alt),
+                          onPressed: () {
+                            _showOverlay(context);
+                          },
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context).translate('descriptionToAdress'),
+                      ),
+                      controller: _controller,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: _width / 2,
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              _error,
+                              style: TextStyle(color: _dataColor)
+                            ),
+                            Text("${AppLocalizations.of(context).translate('localVersion')}: ${this._localVersion ?? "---"}"),
+                            this._adressCorrect ? Text("${AppLocalizations.of(context).translate('onlineVersion')}: ${this._decodedJson['Version']}") : Text("${AppLocalizations.of(context).translate('onlineVersion')}: ---"),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: _width /2,
+                        padding: EdgeInsets.all(15),
+                        child: _button,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
             Divider(),
+            // Header for "Appearance Settings"
             ListTile(
               title: Text(
                 AppLocalizations.of(context).translate('appearanceSettings'),
@@ -387,6 +384,7 @@ class _SettingsWidget extends State<SettingsWidget> {
               },
             ),
             Divider(),
+            // About Area and Links
             ListTile(
               title: Text(
                 AppLocalizations.of(context).translate('aboutHeader'),
